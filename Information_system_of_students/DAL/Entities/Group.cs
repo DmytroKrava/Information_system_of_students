3f4b5706c0e2ccc297f4dcb0db96aa7462761af8
DmytroKrava <93785602+DmytroKrava@users.noreply.github.com>
2021-11-18 22:09:18 +0200

    Student&Group

---
+++ Information_system_of_students/DAL/Entities/Group.cs
@@ -0,0 +1,11 @@
+﻿namespace Information_system_of_students.DAL.Entities
+{
+    public class Group
+    {
+        public int groupID { get; set; }
+        public string Name { get; set; }
+        public string educationalDegree { get; set; }
+        public string speciality { get; set; }
+        public IEnumerable<Student> students { get; set; }
+    }
+}
---
+++ Information_system_of_students/DAL/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Information_system_of_students.DAL.Entities
+{
+    public class Student
+    {
+        public int StudentsID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
