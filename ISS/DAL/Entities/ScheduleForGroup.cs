0b5f18512618e78b528ce8d0de60bc25bcf29f13
DmytroKrava <93785602+DmytroKrava@users.noreply.github.com>
2021-11-25 20:24:49 +0200

    changes

---
+++ ISS/DAL/Entities/ScheduleForGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.Entities
+{
+    public class ScheduleForGroup
+    {
+        public int ScheduleId { get; set; }
+        public string Group { get; set; }
+        public string Description { get; set; }
+    }
+}
